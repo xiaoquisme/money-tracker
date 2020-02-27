79e8e7b1c45672d8a91c41f66d4806369d7b0b4f
XiaoQu <lqqu@thoughtworks.com>
2020-02-27 21:17:12 +0800

    refactor 今日消费 extract report base component

---
+++ miniprogram/pages/component/report-base.js
@@ -0,0 +1,21 @@
+Component({
+    options: {
+        addGlobalClass: true,
+    },
+    properties: {
+      date: String,
+      dateDescription: String,
+      lostItems: Array,
+      lostItemTitle: String,
+      incomeItems: Array,
+      incomeItemsTitle: String,
+      totalLost: Number,
+      totalIncome: Number,
+      total: Number,
+      totalDescription: String,
+    },
+
+    data: {},
+
+    methods: {}
+});
