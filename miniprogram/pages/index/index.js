d5ce172e42e0388f091b29ad28a9169befa7ea2d
XiaoQu <lqqu@thoughtworks.com>
2020-02-29 14:13:18 +0800

    remove useless function

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -45,19 +45,6 @@ Page({
     });
   },
 
-  onLogout: function () {
-    wx.openSetting({
-      success: res => {
-        if (!res.authSetting[ 'scope.userInfo' ]) {
-          app.globalData.logged = false;
-          wx.redirectTo({
-            url: "/pages/login/login",
-          })
-        }
-      },
-    })
-  },
-
   kindToggle: function (e) {
     var id = e.currentTarget.id, list = this.data.functions;
     for (var i = 0, len = list.length; i < len; ++i) {
@@ -71,55 +58,4 @@ Page({
       functions: list,
     });
   },
-
-  // 上传图片
-  doUpload: function () {
-    // 选择图片
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['compressed'],
-      sourceType: ['图库', '拍照'],
-      success: function (res) {
-
-        wx.showLoading({
-          title: '上传中',
-        })
-
-        const filePath = res.tempFilePaths[ 0 ]
-
-        // 上传图片
-        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[ 0 ]
-        wx.cloud.uploadFile({
-          cloudPath,
-          filePath,
-          success: res => {
-            console.log('[上传文件] 成功：', res)
-
-            app.globalData.fileID = res.fileID
-            app.globalData.cloudPath = cloudPath
-            app.globalData.imagePath = filePath
-
-            wx.navigateTo({
-              url: '../storageConsole/storageConsole'
-            })
-          },
-          fail: e => {
-            console.error('[上传文件] 失败：', e)
-            wx.showToast({
-              icon: 'none',
-              title: '上传失败',
-            })
-          },
-          complete: () => {
-            wx.hideLoading()
-          }
-        })
-
-      },
-      fail: e => {
-        console.error(e)
-      }
-    })
-  },
-
 });
