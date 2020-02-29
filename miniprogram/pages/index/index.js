f30bf3cb3434c66619f7e5ff56f8071f9794d3a4
XiaoQu <lqqu@thoughtworks.com>
2020-02-29 13:58:01 +0800

    refactor today

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -23,7 +23,7 @@ Page({
         name: "其他",
         open: false,
         pages: [
-          { name: "今日消费", url: "/pages/today/cost" },
+          { name: "今日消费", url: "/pages/today/today" },
           { name: "本周消费", url: "/pages/week/week" }
         ]
       }
