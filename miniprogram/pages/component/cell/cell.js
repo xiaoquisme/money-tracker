fc38ac5635f3dd65052f545446582baf93e99b9e
XiaoQu <lqqu@thoughtworks.com>
2020-03-20 15:40:49 +0800

    add left slide to delete item.

---
+++ miniprogram/pages/component/cell/cell.js
@@ -0,0 +1,44 @@
+// pages/component/cell/cell.js
+Component({
+    options: {
+        addGlobalClass: true,
+    },
+
+    properties: {
+        item: Object,
+    },
+
+    data: {
+        slideButtons: [],
+    },
+
+    ready: function () {
+        this.setData({
+            slideButtons: [
+                {
+                    type: 'warn',
+                    text: '删除',
+                    data: this.properties.item,
+                    // extClass: 'weui-cell',
+                    src: '/page/weui/cell/icon_del.svg', // icon的路径
+                }
+            ]
+        })
+    },
+
+    methods: {
+        lostItemsDelete: function (event) {
+            const { data } = event.detail;
+            this.triggerEvent('customevent', { data: data._id }, { bubbles: true });
+            wx.cloud.callFunction({
+                name: "db",
+                data: {
+                    type: "delete-money-tracker",
+                    data: {
+                        id: data._id
+                    }
+                }
+            }).then(res => {})
+        }
+    },
+})
--- miniprogram/pages/component/report-base.js
+++ miniprogram/pages/component/report-base.js
@@ -3,19 +3,26 @@ Component({
         addGlobalClass: true,
     },
     properties: {
-      date: String,
-      dateDescription: String,
-      lostItems: Array,
-      lostItemsTitle: String,
-      incomeItems: Array,
-      incomeItemsTitle: String,
-      totalLost: Number,
-      totalIncome: Number,
-      total: Number,
-      totalDescription: String,
+        date: String,
+        dateDescription: String,
+        lostItems: Array,
+        lostItemsTitle: String,
+        incomeItems: Array,
+        incomeItemsTitle: String,
+        totalLost: Number,
+        totalIncome: Number,
+        total: Number,
+        totalDescription: String,
     },
 
     data: {},
 
-    methods: {}
+    methods: {
+        deleteItemCallBack: function (event) {
+            const id = event.detail.data;
+            this.setData({
+                lostItems:this.data.lostItems.filter(i => i._id !== id),
+            });
+        }
+    }
 });
