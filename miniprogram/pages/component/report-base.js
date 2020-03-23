23310423d00e4d7d318daf011671647dc887ffb4
XiaoQu <lqqu@thoughtworks.com>
2020-03-23 22:44:38 +0800

    fix income not working when delete

--- miniprogram/pages/component/report-base.js
+++ miniprogram/pages/component/report-base.js
@@ -17,10 +17,22 @@ Component({
 
     methods: {
         deleteItemCallBack: function (event) {
-            const id = event.detail.data;
-            this.setData({
-                lostItems: this.data.lostItems.filter(i => i._id !== id),
-            });
-        }
+            const updateLostItem = id =>
+                this.setData({
+                    lostItems: this.data.lostItems.filter(i => i._id !== id),
+                });
+            const updateIncomeItem = id =>
+                this.setData({
+                    incomeItems: this.data.incomeItems.filter(i => i._id !== id),
+                });
+            const { id, type } = event.detail.data;
+            if (type === "INCOME") {
+                updateIncomeItem(id);
+            }
+            if (type === "LOST") {
+                updateLostItem(id);
+            }
+        },
+
     }
 });
