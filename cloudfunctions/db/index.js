268377c31343a5b369f7a1db71851e607aa60700
XiaoQu <lqqu@thoughtworks.com>
2020-03-30 21:18:56 +0800

    add year and month field

--- cloudfunctions/db/index.js
+++ cloudfunctions/db/index.js
@@ -1,5 +1,6 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk');
+const _ = require('lodash');
 
 cloud.init();
 const db = cloud.database();
@@ -19,27 +20,30 @@ exports.main = async (event, context) => {
     if (type === "money-types-lost") {
         return await findAllMoneyLostTypes();
     }
-    if(type === "money-types-income") {
+    if (type === "money-types-income") {
         return await findAllMoneyIncomeTypes();
     }
     if (type === "money-types-add") {
         return await addMoneyType(data);
     }
-    if(type === "delete-money-tracker"){
+    if (type === "delete-money-tracker") {
         return await deleteMoneyTracker(data);
     }
-    if(type === "add-money-tracer"){
+    if (type === "add-money-tracer") {
         return await addMoneyTracker(data);
+    }
+    if(type === 'refreshDataWithMonthAndYear'){
+        return await refreshDataWithMonthAndYear(data);
     }
     return [];
 };
 
 async function addMoneyTracker(data) {
-    const {date} = data;
+    const { date } = data;
     const weekNumber = await getWeekNumber(date);
     return db.collection("money-tracker")
         .add({
-            data: {...data, isDelete: false, weekNumber}
+            data: { ...data, isDelete: false, weekNumber }
         })
 }
 
@@ -102,8 +106,56 @@ async function getWeekData(weekNumber) {
         }).get();
 }
 
+async function getAllData() {
+    const totalCount = await db.collection("money-tracker").count();
+    const batchTimes = Math.ceil(totalCount / 100);
+    const tasks = [];
+    for (let page in batchTimes) {
+        const promise = db.collection("money-tracker").skip(page * 100).limit(100).get();
+        tasks.push(promise);
+    }
+    return (await Promise.all(tasks)).reduce((acc, currentValue) => {
+        return {
+            data: acc.data.concat(currentValue.data),
+            errorMessage: acc.errorMessage,
+        }
+    }, { data: null, errorMessage: null })
+}
+
+async function refreshDataWithMonthAndYear(weekNumber) {
+    const response = await db.collection("money-tracker").get();
+    return (await Promise.all(response.data.map(d => {
+        return {
+            ...d,
+            month: getMonth(d.date),
+            year: getYear(d.date)
+        }
+    }).map(item => {
+        return db.collection("money-tracker")
+            .where({
+                _id: item._id
+            })
+            .update({
+                data: _.omit(item, '_id'),
+            });
+    })));
+
+}
+
+function getMonth(date) {
+    return parseInt(date.split('-')[1]);
+}
+
+function getYear(date) {
+    return parseInt(date.split('-')[0])
+}
+
+function getToday() {
+    return new Date().toISOString().split('T')[0];
+}
+
 async function deleteTodayTestData() {
-    const today = new Date().toISOString().split('T')[ 0 ];
+    const today = getToday();
     return db.collection("money-tracker")
         .where({
             date: today,
