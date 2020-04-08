a2b27db1762b606549b2139ad41c69937122bbf8
XiaoQu <lqqu@thoughtworks.com>
2020-04-08 21:55:13 +0800

    refactor: add eslint auto fix command

--- cloudfunctions/db/__tests__/utils.test.js
+++ cloudfunctions/db/__tests__/utils.test.js
@@ -1,39 +1,39 @@
 const utils = require('../src/utils');
 
-describe("utils", () => {
-    describe("getWeekNumber", () => {
+describe('utils', () => {
+    describe('getWeekNumber', () => {
         it('should get WeekNumber correctly', async function () {
             const cloudFunction = jest.fn().mockResolvedValue({ result: { weekNumber: 1 } });
             const cloud = {
                 callFunction: cloudFunction,
             };
-            const result = await utils.getWeekNumber(cloud, "2020-01-01");
+            const result = await utils.getWeekNumber(cloud, '2020-01-01');
             expect(result).toBe(1);
         });
     });
 
     describe('getToday', () => {
         it('should return yyyy-MM-dd format date', () => {
-            const timeStamp = new Date("2011-01-01").getTime();
+            const timeStamp = new Date('2011-01-01').getTime();
             jest.spyOn(Date, 'now').mockImplementation(() => timeStamp);
             const today = utils.getToday();
-            expect(today).toBe("2011-01-01");
+            expect(today).toBe('2011-01-01');
         });
     });
 
-    describe("getMonth", () => {
+    describe('getMonth', () => {
         it('should get 1 with date: 2020-01-01', function () {
-            expect(utils.getMonth("2020-01-01")).toBe(1);
+            expect(utils.getMonth('2020-01-01')).toBe(1);
         });
     });
 
-    describe("getYear", () => {
+    describe('getYear', () => {
         it('should get 2020 with date: 2020-01-01', function () {
-            expect(utils.getYear("2020-01-01")).toBe(2020);
+            expect(utils.getYear('2020-01-01')).toBe(2020);
         });
     });
 
-    describe("getDatabase", () => {
+    describe('getDatabase', () => {
         it('should getDatabase', function () {
             const mockFunc = jest.fn().mockReturnValue(1);
             const cloud = {
@@ -44,7 +44,7 @@ describe("utils", () => {
         });
     });
 
-    describe("getUserId", () => {
+    describe('getUserId', () => {
         it('should getUserId', function () {
             const mockFunc = jest.fn().mockReturnValue({ OPENID: 1 });
             const cloud = {
