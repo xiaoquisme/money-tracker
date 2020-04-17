6c09ccc1122368ae127b3c10a254079f4931c243
XiaoQu <lqqu@thoughtworks.com>
2020-04-17 22:51:12 +0800

    feat: change test to use test db

--- cloudfunctions/db/__tests__/moneyTracker.test.js
+++ cloudfunctions/db/__tests__/moneyTracker.test.js
@@ -10,18 +10,16 @@ describe('moneyTrackerTest', () => {
             utils.getMonth.mockReturnValue(1);
             utils.getYear.mockReturnValue(2020);
             utils.getUserId.mockReturnValue('userId');
-            const mockAdd = jest.fn().mockResolvedValue(1);
-            const mockCollection = jest.fn().mockReturnValue({ add: mockAdd });
-            const mockDB = jest.fn().mockReturnValue({ collection: mockCollection });
-
+            const mockDB = mockCloudDB('success');
             utils.getDatabase.mockReturnValue(mockDB());
             const cloud = jest.fn().mockReturnValue(null);
             const data = {
                 date: '2020-01-01',
             };
             const response = await moneyTracker.addMoneyTracker(data, cloud);
-            expect(response).toBe(1);
-            expect(mockAdd).toBeCalledWith({
+            expect(response).toBe('success');
+            expect(mockDB().collection).toBeCalledWith('money-tracker');
+            expect(mockDB().collection().add).toBeCalledWith({
                 data: {
                     _openid: 'userId',
                     date: '2020-01-01',
@@ -36,19 +34,16 @@ describe('moneyTrackerTest', () => {
 
     describe('deleteMoneyTracker', () => {
         it('should delete MoneyTrackerData', async function () {
-            const mockDBUpdate = jest.fn().mockResolvedValue('success');
-            const mockDBWhere = jest.fn().mockReturnValue({ update: mockDBUpdate });
-            const mockDBCollection = jest.fn().mockReturnValue({ where: mockDBWhere });
-            const mockDB = jest.fn().mockReturnValue({ collection: mockDBCollection });
+            const mockDB = mockCloudDB('success');
             utils.getDatabase.mockReturnValue(mockDB());
             const data = {
                 id: 1
             };
             const response = await moneyTracker.deleteMoneyTracker(data, null);
             expect(response).toBe('success');
-            expect(mockDBCollection).toBeCalledWith('money-tracker');
-            expect(mockDBWhere).toBeCalledWith({ _id: 1 });
-            expect(mockDBUpdate).toBeCalledWith({
+            expect(mockDB().collection).toBeCalledWith('money-tracker');
+            expect(mockDB().collection().where).toBeCalledWith({ _id: 1 });
+            expect(mockDB().collection().where().update).toBeCalledWith({
                 data: {
                     isDelete: true
                 }
@@ -62,7 +57,7 @@ describe('moneyTrackerTest', () => {
             jest.clearAllMocks();
         });
         it('should get only me data when onlyMe = true', async function () {
-            const { mockDB } = mockCloudDB('success');
+            const mockDB = mockCloudDB('success');
             utils.getDatabase.mockReturnValue(mockDB());
             utils.getUserId.mockReturnValue('userId');
             const response = await moneyTracker.getDayData('day', 'any', true);
@@ -74,7 +69,7 @@ describe('moneyTrackerTest', () => {
         });
 
         it('should get all data when onlyMe=false', async function () {
-            const { mockDB } = mockCloudDB('success');
+            const mockDB = mockCloudDB('success');
             utils.getDatabase.mockReturnValue(mockDB());
             const response = await moneyTracker.getDayData('day', 'any', false);
             expect(response).toBe('success');
--- cloudfunctions/db/cloudDBTestUtils.js
+++ cloudfunctions/db/cloudDBTestUtils.js
@@ -1,13 +1,29 @@
-function mockCloudDB(responseValue, serverDateInit = '2020-01-01') {
-    const mockResponse = jest.fn().mockImplementation(() => responseValue);
-    const mockWhere = jest.fn().mockReturnValue({ get: mockResponse });
-    const mockCollection = jest.fn().mockReturnValue({ where: mockWhere });
-    const mockCreateCollection = jest.fn().mockReturnValue('promise');
+function getMockServerDate(serverDateInit) {
     const mockServerDate = jest.fn().mockImplementation(({ offset }) => {
         const date = new Date(serverDateInit);
         date.setMilliseconds(offset);
         return date;
     });
+    return mockServerDate;
+}
+
+function getMockCreateCollection() {
+    return jest.fn().mockResolvedValue('todo: maybe succes');
+}
+
+function getMockCollection(responseValue) {
+    const mockResponse = jest.fn().mockImplementation(() => responseValue);
+    const mockUpdate = jest.fn().mockReturnValue(responseValue);
+    const mockWhere = jest.fn().mockReturnValue({ get: mockResponse, update: mockUpdate });
+    const mockAdd = jest.fn().mockReturnValue(responseValue);
+    const mockCollection =  jest.fn().mockReturnValue({ where: mockWhere, add: mockAdd });
+    return mockCollection;
+}
+
+function mockCloudDB(responseValue, serverDateInit = '2020-01-01') {
+    const mockCollection = getMockCollection(responseValue);
+    const mockCreateCollection = getMockCreateCollection();
+    const mockServerDate = getMockServerDate(serverDateInit);
     const mockRunTransaction = jest.fn().mockReturnValue('todo promise');
     const mockStartTransaction = jest.fn().mockReturnValue('promise');
     const mockDB = jest.fn().mockReturnValue({
@@ -17,9 +33,7 @@ function mockCloudDB(responseValue, serverDateInit = '2020-01-01') {
         runTransaction: mockRunTransaction,
         startTransaction: mockStartTransaction
     });
-    return {
-        mockDB
-    };
+    return mockDB;
 }
 
 module.exports = {
