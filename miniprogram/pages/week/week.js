cddb17cee81cb922c870ccf21b81df31f64123b6
XiaoQu <lqqu@thoughtworks.com>
2020-05-10 16:57:07 +0800

    refactor: fix eslint

--- miniprogram/pages/week/week.js
+++ miniprogram/pages/week/week.js
@@ -3,34 +3,35 @@ Page({
 
     data: {
         week: null,
-        weekDescription: "周账单",
+        weekDescription: '周账单',
         lostItems: [],
-        lostItemsTitle: "周消费",
+        lostItemsTitle: '周消费',
         incomeItems: [],
-        incomeItemsTitle: "周收入",
+        incomeItemsTitle: '周收入',
         weekLost: 0,
         weekIncome: 0,
-        totalDescription: "累计消费",
+        totalDescription: '累计消费',
         showActionSheet: true,
         groups: [
-            "choice-day",
+            'choice-day',
         ],
         weekNumberOptions: [],
         onlyMe: false,
     },
 
+    // eslint-disable-next-line no-unused-vars
     onLoad: function (options) {
         wx.showLoading({ title: '数据加载中' });
         wx.cloud.callFunction({
-            name: "lib",
+            name: 'lib',
             data: { date: getToday() },
         }).then(res => res.result).then(res => {
             this.setData({
                 weekNumberOptions: [...Array(parseInt(res.weekNumber) + 1).keys()].reverse()
-            })
-        }).then(any => {
+            });
+        }).then(() => {
             wx.hideLoading();
-        })
+        });
     },
     onWeekSelect: function (e) {
         const selectedWeekNumber = this.data.weekNumberOptions[e.detail.value];
@@ -40,13 +41,13 @@ Page({
         });
         this.loadData(selectedWeekNumber);
     },
-    closeActionSheet: function (e) {
+    closeActionSheet: function () {
         this.setData({
             showActionSheet: false,
         });
         wx.navigateBack({
             delta: -1
-        })
+        });
     },
     onOnlyMeChange: function (e) {
         this.setData({
@@ -56,15 +57,15 @@ Page({
     loadData: function (selectedWeekNumber) {
         wx.showLoading({ title: '数据加载中' });
         wx.cloud.callFunction({
-            name: "db",
+            name: 'db',
             data: {
-                type: "week",
+                type: 'week',
                 date: selectedWeekNumber,
                 onlyMe: this.data.onlyMe,
             }
         }).then(res => res.result).then(res => {
-            const lostItems = res.data.filter(d => d.moneyType === "LOST").reverse();
-            const incomeItems = res.data.filter(d => d.moneyType === "INCOME").reverse();
+            const lostItems = res.data.filter(d => d.moneyType === 'LOST').reverse();
+            const incomeItems = res.data.filter(d => d.moneyType === 'INCOME').reverse();
             const weekLost = lostItems.reduce((pre, cur) => parseFloat(cur.count) + pre, 0);
             const weekIncome = incomeItems.reduce((pre, cur) => parseFloat(cur.count) + pre, 0);
             this.setData({
@@ -74,7 +75,7 @@ Page({
                 week: selectedWeekNumber,
                 weekIncome: weekIncome,
             });
-        }).then(any => {
+        }).then(() => {
             wx.hideLoading();
         });
     }
