d75b7166a4330dd2bf9a0c463a73eb29eeb8b42a
XiaoQu <lqqu@thoughtworks.com>
2020-06-04 16:59:20 +0800

    refactor: move jest config to single file

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const { defaults } = require('jest-config');
+module.exports = {
+    moduleFileExtensions: [...defaults.moduleFileExtensions, 'js'],
+    collectCoverage: true,
+    coverageDirectory: './coverage/',
+    testRegex: '__tests__',
+    coverageThreshold: {
+        global: {
+            branches: 0,
+            functions: 0,
+            lines: 0,
+            statements: 0
+        }
+    },
+    transform: { '^.+\\.[j]s?$': 'babel-jest' }
+};
